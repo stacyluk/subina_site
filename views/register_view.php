80595a90482bc7ad53822e3faaea362a6676508e
Nastya Lukyanova <anastasiya.look@gmail.com>
2019-12-17 00:28:00 +0700

    changed main.php

--- views/register_view.php
+++ views/register_view.php
@@ -6,7 +6,7 @@
         <p><label for="user_pass">E-mail<br>
                 <input class="input" id="email" name="email" size="32"type="email" value=""></label></p>
         <p><label for="user_pass">Имя пользователя<br>
-                <input class="input" id="username" name="username"size="20" type="text" value=""></label></p>
+                <input class="input" id="username" name="username"size="32" type="text" value=""></label></p>
         <p><label for="user_pass">Пароль<br>
                 <input class="input" id="password" name="password"size="32"   type="password" value=""></label></p>
         <p class="submit"><input class="button" id="register" name= "register" type="submit" value="Зарегистрироваться"></p>
